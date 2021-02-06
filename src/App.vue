c2c560374e9aa12b5f6d64567f94cac376c8e653
matthew.chevy <matej.sevcik11@gmail.com>
2021-02-06 10:22:57 +0100

    add router link

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+    <header>
+        <navigation />
+    </header>
+
+    <background-image v-show="actualyWidnowSize > breakPointExtraLarge" />
+
+    <router-link class="hidden" to="/">home</router-link>
+    <!-- Is good this using ? -->
+    <router-link class="hidden" to="/about">about</router-link>
+    <router-link class="hidden" to="/contact">contact</router-link>
+    <router-link class="hidden" to="/equipment">equipment</router-link>
+    <router-link class="hidden" to="/gallery">gallery</router-link>
+    <router-view />
+
 </template>
 
+<script>
+export default {
+  
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
