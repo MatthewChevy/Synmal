4a40a287ea5b1c10771aa55193c76857859d15d8
matthew.chevy <matej.sevcik11@gmail.com>
2021-02-08 12:17:26 +0100

    menu animatio - small screens

--- src/components/TheNavigationLogo.vue
+++ src/components/TheNavigationLogo.vue
@@ -17,7 +17,14 @@
 .container{
     padding-top: 0.25rem;
     max-width: 7rem;
-}   
+    animation: logo-smooth  1s ease-in-out;
+    animation-fill-mode:forwards
+} 
+
+@keyframes logo-smooth {
+         0% { transform: translateX(-100%); }
+        100% { transform: translateX(0%); }
+}
 
 img{
     max-width: 100%;
@@ -42,13 +49,59 @@ img{
         max-width: 14.6rem;
         top: -0.4rem;
         left: 1rem;
-        animation: ready 1s ease-in-out;
+        animation: logo-smooth-xl  1s ease-in-out;
         animation-fill-mode:forwards
     }
 
-    @keyframes ready {
+    @keyframes logo-smooth-xl  {
          0% { transform: translateX(-15%); }
         100% { transform: translateX(0%); }
+    }
+}
+
+@media screen and ( min-width:$w-2xl){
+    .container{
+        position: absolute;
+        max-width: 16rem;
+        top: -0.8rem;
+        left: 1rem;
+        animation: logo-smooth-2xl 1s ease-in-out;
+        animation-fill-mode:forwards
+    }
+
+    @keyframes logo-smooth-2xl  {
+         0% { transform: translateX(-15%); }
+        100% { transform: translateX(5%); }
+    }
+}
+
+@media screen and ( min-width:$w-3xl){
+    .container{
+        max-width: 16.8rem;
+        top: -0.8rem;
+        left: 1rem;
+        animation: logo-smooth-3xl 1s ease-in-out;
+        animation-fill-mode:forwards
+    }
+
+    @keyframes logo-smooth-3xl{
+         0% { transform: translateX(-15%); }
+        100% { transform: translateX(25%); }
+    }
+}
+
+@media screen and ( min-width:$w-4k){
+    .container{
+        max-width: 34rem;
+        top: -1.35rem;
+        left: 1rem;
+        animation: logo-smooth-4k  1s ease-in-out;
+        animation-fill-mode:forwards
+    }
+
+    @keyframes logo-smooth-4k  {
+         0% { transform: translateX(-15%); }
+        100% { transform: translateX(50%); }
     }
 }
 
