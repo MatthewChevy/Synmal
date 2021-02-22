6f41088569107dfd4c4f11e0d8ce5186ea84730e
matthew.chevy <matej.sevcik11@gmail.com>
2021-02-22 20:40:30 +0100

    navigation for small screen

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -3,8 +3,6 @@
         <navigation-logo />
         <navigation-menu-button @toggle-menu="toggleMenu($event)" />
     </div>
-    <!-- <navigation-social-bar /> -->
-
     <nav>
         <ul v-if="activeMenu || breakPointLarge < actualyWidnowSize">
             <li tabindex="1" @click="$router.push({ path: '/' })">DOMOV</li>
@@ -25,7 +23,6 @@
 <script>
 import tableMixin from '../mixins/tableMixins'
 
-// import NavigationSocialBar from './TheNavigationSocialBar.vue'
 import NavigationLogo from './TheNavigationLogo'
 import NavigationMenuButton from './TheNavigationMenuButton.vue'
 
@@ -58,117 +55,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.banner {
+
+.banner{
     display: flex;
     justify-content: space-between;
-    padding-bottom: 0.2rem;
+    background: #373737 
 }
 
-nav {
-    min-width: 100vw;
-    position: absolute;
-    z-index: 10;
-    font-family: $font-family-sans-serif;
-    font-weight: $font-weight-navigation;
-    letter-spacing: 1px;
+nav{
+    position: relative;
+    width: 100%;
+    background: $light-grey;
+    color: $black;
     text-align: center;
-
-    ul {
-        background: $navigation-background-color;
-
-        li:first-child {
-            border-top: 1px solid $navigation-border-color;
-        }
-
-        li {
-            background: $navigation-background-color;
-            color: $navigation-text-color;
-            border-bottom: 1px solid $navigation-text-color;
-            padding: {
-                top: 0.9em;
-                bottom: 0.9rem;
-            }
-        }
-
-        li:hover,
-        :focus {
-            transition: 0.3s;
-            background: rgb(141, 141, 141);
-            color: $navigation-background-color;
-        }
-    }
+    
 }
 
-@media screen and (min-width: $w-lg) {
-    nav {
-        z-index: 1;
-        top: 1.9rem;
 
-        ul {
-            li:first-child {
-                border-top: none;
-            }
-
-            li {
-                padding: 0rem 1.5rem;
-                border-bottom: none;
-                display: inline-block;
-            }
-        }
+li {
+    font:{
+        size: 14px;
+        weight: 500;
     }
+    padding: 15px 0px;
+    border-bottom: 1px solid $dark;
 }
 
-@media screen and (min-width: $w-xl) {
-    nav {
-        ul {
-            border-radius: 0.5rem;
-            max-width: 95%;
-            margin: 0 auto;
-
-            li {
-                padding: 1.3rem 2rem;
-            }
-
-            li:hover,
-            :focus {
-                border-radius: 0.5rem;
-                transition: 0.5s;
-            }
-        }
-    }
-}
-
-@media screen and (min-width: $w-3xl) {
-    nav {
-        font-size: 1rem;
-        ul {
-            li {
-                padding: 1.6rem 3rem;
-            }
-        }
-    }
-}
-
-@media screen and (min-width: $w-4xl) {
-    nav {
-        font-size: 1.2rem;
-        ul {
-            li {
-                padding: 1.4rem 4rem;
-            }
-        }
-    }
-}
-
-@media screen and (min-width: $w-4k) {
-    nav {
-        top: 3.7rem;
-        font-size: 2.5rem;
-        ul {
-            li {
-                padding: 2.8rem 5.5rem;
-            }
-        }
-    }
-}
 </style>
