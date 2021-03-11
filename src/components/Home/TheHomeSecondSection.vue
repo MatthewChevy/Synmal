68b39d8af449d4b28f1253bb01342dc8c1beb95c
matthew.chevy <matej.sevcik11@gmail.com>
2021-03-11 16:07:02 +0100

    new components, second section, 0 to 640, 640+

--- src/components/Home/TheHomeSecondSection.vue
+++ src/components/Home/TheHomeSecondSection.vue
@@ -1,402 +1,146 @@
 <template>
     <section class="second-section">
-        <div class="second-section-z-index-background"></div>
         <div class="second-section-background">
             <div class="second-section-container">
                 <div class="second-section-h3-handler">
                     <h3>
-                        Keďže aj my ideme s dobou, ponúkame veľmi populárne a moderné
-                        strojové maľovanie interiérov a exteriérov
+                        Keďže aj my ideme s dobou, ponúkame veľmi populárne a
+                        moderné strojové maľovanie interiérov a exteriérov.
                     </h3>
                 </div>
             </div>
-                <div class="second-section-wagner-color-splitter">
-                    <div class="second-section-wagner-container">
-                        <div class="second-section-wagner">
-                            <div class="paint-machine-about">
-                                <div class="second-section-logo-holder">
-                                    <img
-                                        src="../../assets/img/wagner.png"
-                                        alt="wagner logo"
-                                    />
-                                </div>
-                                <div class="second-section-paragraph-holder">
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing
-                                        elit, sed do eiusmod tempor incididunt ut labore et
-                                        dolore magna aliqua. Ut enim ad minim veniam, quis
-                                        nostrud exercitation ullamco laboris.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="second-section-top-image">
-                                <img
-                                    src="../../assets/img/airless-painting.jpg"
-                                    alt="Airless painting"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="second-section-bottom-handler">
-                    <div class="second-section-bottom-image">
-                        <img
-                            src="../../assets/img/wagner-machine.jpg"
-                            alt="Wagner Machine"
-                        />
-                    </div>
-                </div>
-            </div>
+
+            <the-home-second-section-article-sm class="article-sm" />
+            <the-home-second-section-article class="article-md" />
+
             <aside class="second-section-decorative-line"></aside>
+        </div>
     </section>
 </template>
 
 <script>
-    export default {
-        
-    }
+import tableMixins from '../../mixins/tableMixins.js'
+
+import TheHomeSecondSectionArticleSm from './TheHomeSecondSectionArticleSm.vue'
+import TheHomeSecondSectionArticle from './TheHomeSecondSectionArticle.vue'
+
+export default {
+    mixins: [tableMixins],
+
+    components: {
+        TheHomeSecondSectionArticleSm,
+        TheHomeSecondSectionArticle
+    },
+
+}
 </script>
 
 <style lang="scss" scoped>
 
-//second section-------------------------------------------
+@media screen and (min-width: 0px) {
 
-.second-section{
-    position: relative;
-    width: 100%;
-    z-index: 3;
-    background-color: $primary-light;
-}
-
-.second-section-z-index-background{
-    position: absolute;
-    width: 100%;
-    height: 122%;
-    top: -2rem;
-    z-index: 0;
-    background-color: $primary-light;
-}
-
-.second-section-background{
-    position: relative;
-    width: 100%;
-    background-color: $primary-semi-dark;
-}
-
-.second-section-container{
-    width: 95%;
-    margin: 0 auto;
-}
-
-.second-section-h3-handler{
-    max-width: 97%;
-    padding: 1rem 0;
-
-    h3{
-        color: $primary-light;
-        font:{
-            size: 13.5px;
-            weight: 400;
-        }
-    }
-}
-
-.second-section-wagner-color-splitter{
-    position: relative;
-    width: 100%;
-    z-index: 1;
-    background-color: $primary;
-}
-
-.second-section-wagner-container{
-    width: 95%;
-    margin: 0 auto;
-    padding: 3% 0 7% 0;
-}
-
-.second-section-wagner{
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    z-index: 1;
-}
-
-.paint-machine-about{
-    width: 49%;
-}
-
-.second-section-logo-holder{
-    width: 87%;
-    img{
-        max-width:100%;
-    }
-}
-
-.second-section-paragraph-holder{
-    max-width: 100%;
-    padding-top: 5%;
-    p{
-        font:{
-            size: 10px;
-            weight: 400;
-        }
-    }
-}
-
-.second-section-top-image{
-    width: 49%;
-    height: fit-content;
-    margin-top: 6%;
-    border-radius: 3px;
-    line-height: 0;
-    border: 3px solid $primary-light;
-    box-shadow: 5px 5px 3px rgba($darkest, 16%);
-
-    img{
-        max-width:100%;
-    }
-}
-
-.second-section-bottom-handler{
-    max-height: 6rem;
-}
-
-.second-section-bottom-image{
-    position: relative;
-    width: 76%;
-    top: -1rem;
-    margin: 0 auto;
-    border-radius: 3px;
-    line-height: 0;
-    z-index: 3;
-    border: 3px solid $primary-light;
-    box-shadow: 5px 5px 3px rgba($darkest, 16%);
-
-    img{
-        max-width:100%;
-    }
-}
-
-.second-section-decorative-line{
-    position: absolute;
-    width: 0.313rem;
-    height: 100%;
-    top: 0;
-    right: 0.813rem;
-    z-index: 0;
-    background-color: $fancy;
-    box-shadow: 5px 0px 2px rgb(0 0 0 / 16%); 
-}
-
-@media screen and ( min-width: 375px){
-
-    .second-section{
+    .second-section {
         position: relative;
-        display: flex;
-        justify-content: flex-end;
         width: 100%;
+        margin-top: -2rem;
+        padding-top: 2rem;
+        z-index: 2;
         background-color: $primary-light;
     }
 
-    .second-section-background{
+    .second-section-background {
+        position: relative;
+        width: 100%;
+        background-color: $primary-semi-dark;
+        box-shadow: 7px 7px 3px rgba($darkest, 16%);
+    }
+
+    .second-section-container {
         width: 95%;
-        border-radius:10px 0 0 10px ;
+        margin: 0 auto;
     }
 
-    .second-section-h3-handler{
-    max-width: 95%;
-    padding: 1rem 0;
+    .second-section-h3-handler {
+        max-width: 87%;
+        padding: 1.25rem 0;
+        text-align: left;
 
-    h3{
-        color: $primary-light;
-        font:{
-            size: 14px;
-            weight: 400;
-        }
-    }
-}
-
-    .second-section-paragraph-holder{
-        p{
-            font:{
-                size: 11px;
+        h3 {
+            color: $primary-light;
+            font: {
+                size: 13.5px;
+                weight: 400;
             }
         }
-    }
-}
-
-@media screen and ( min-width: 400px){
-    .second-section-container{
-        width: 93%;
-    }
-
-    .second-section-wagner-container {
-        width: 85%;
-    }
-
-    .second-section-bottom-image{
-        width: 70%;
-    }
-}
-
-@media screen and ( min-width: 500px) {
-
-    .second-section-z-index-background{
-        height: 46rem;
-    }
-
-    .second-section-h3-handler{
-        h3{
-            font:{
-                size: 16px;
-            }
-        }
-    }
-
-    .second-section-paragraph-holder{
-        p{
-            font:{
-                size: 12px;
-            }
-        }
-    }
-
-}
-
-@media screen and (min-width: 550px){
-
-    .second-section-paragraph-holder {
-        p{
-            font-size: 13px;
-        }
-    }
-}
-
-@media screen and ( min-width: 600px) {
-
-    .second-section-h3-handler{
-        max-width: 29rem;
-
-        h3{
-            font:{
-                size: 18px;
-            }
-        }
-    }
-
-    .second-section-paragraph-holder{
-        p{
-            font:{
-                size: 14px
-            }
-        };
-    }
-
-    .second-section-top-image{
-        border: 5px solid $primary-light;
-    }
-
-    .second-section-bottom-image{
-        width: 60%;
-        top: -1.5rem;
-        border: 5px solid $primary-light;
-    }
-}
-
-@media screen and ( min-width: 700px) {
-
-    .second-section-h3-handler{
-        max-width: 30rem;
-
-        h3{
-            font:{
-                size: 20px;
-            };
-        }
-    }
-
-    .second-section-wagner-container{
-        padding: 0 0 7% 0;
-    }
-
-    .paint-machine-about {
-        width: 63%;
-        margin-top: 3rem;
-    }
-
-    .second-section-logo-holder{
-        width: 65%;
-    }
-
-    .second-section-paragraph-holder{
-        p{
-            font-size: 15px;
-        }
-    }
-
-    .second-section-bottom-image{
-        top: -1.9rem;
     }
 
     .second-section-decorative-line{
-        width: 0.625rem;
-        right: 1rem;
-    }
-}
-
-@media screen and ( min-width: 768px) {
-
-    .second-section-z-index-background{
-        height: 55rem; 
-    }
-
-}
-
-@media screen and ( min-width: 800px) {
-
-    .paint-machine-about{
-        margin-top: 3rem;
-    }
-
-    .second-section-paragraph-holder{
-        max-width: 86%; 
+        position: absolute;
+        width: 0.313rem;
+        height: 100%;
+        top: 0;
+        right: 0.813rem;
+        z-index: 0;
+        background-color: $fancy;
+        box-shadow: 5px 0px 2px rgb( $darkest, 16%);
     }
 
 }
 
-@media screen and ( min-width: 900px) {
+@media screen and (min-width: 375px) {
 
-    .second-section-h3-handler{
-        max-width: 45rem;
+    .second-section {
+        display: flex;
+        justify-content: flex-end;
+    }
 
-        h3{
-            font-size: 26px;
+    .second-section-background {
+        width: 95%;
+        border-radius: 10px 0 0 10px;
+    }
+
+    .second-section-h3-handler {
+        h3 {
+            color: $primary-light;
+            font: {
+                size: 14px;
+                weight: 400;
+            }
         }
     }
+}   
 
-    .second-section-paragraph-holder{
-        p{
-            font:{
-                size: 20px
+@media screen and (min-width: 550px) {
+
+    .second-section-background {
+        width: 90%;
+    }
+}
+
+@media screen and (min-width: 640px) {
+
+    .second-section {
+        height: auto;
+    }
+
+    .second-section-background {
+        width: 100%;
+        height: auto;
+        border-radius: 0;
+    }
+
+    .second-section-container {
+        width: 85%;
+    }
+
+    .second-section-h3-handler {
+        width: 100%;
+        h3 {
+            font: {
+                size: 20px;
             }
         }
     }
 
-    .second-section-z-index-background{
-        height: 65rem;
-    }
-
-    .third-section-decorative-rectangle{
-        top: 15.55rem; 
-    }
 }
-
-@media screen and ( min-width: 1000px) {
-    .second-section-h3-handler{
-        max-width: 41rem;
-    }
-}
-
-
-
 </style>
---
+++ src/components/Home/TheHomeSecondSectionArticleSm.vue
@@ -0,0 +1,156 @@
+<template>
+    <article class="article">
+        <div class="second-section-equipment-container">
+            <div class="second-section-equipment">
+                <div class="paint-machine-about">
+                    <div class="paint-machine-logo">
+                        <img
+                            src="../../assets/img/wagner.png"
+                            alt="wagner logo"
+                        />
+                    </div>
+                    <div class="second-section-paragraph-holder">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris.
+                        </p>
+                    </div>
+                </div>
+                <div class="second-section-top-image">
+                    <img
+                        src="../../assets/img/gallery/131930597_3221521607952808_6118545964526696010_n.jpg"
+                        alt="Airless painting"
+                    />
+                </div>
+            </div>
+        </div>
+        <div class="second-section-bottom-image-wrapper">
+            <div class="second-section-bottom-image">
+                <img
+                    src="../../assets/img/wagner-machine.jpg"
+                    alt="Wagner Machine"
+                />
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+@media screen and ( min-width: 0px) {
+
+    .article{
+        display: block;
+    }
+    
+    .second-section-equipment-container {
+        position: relative;
+        width: 100%;
+        background-color: $primary;
+        z-index: 2;
+    }
+
+    .second-section-equipment {
+        position: relative;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        width: 97%;
+        margin: 0 auto;
+        padding: 1rem 0 1rem 0.25rem;
+        z-index: 2;
+    }
+
+    .paint-machine-about {
+        width: 50%;
+    }
+
+    .paint-machine-logo {
+        width: 80%;
+        img {
+            max-width: 100%;
+        }
+    }
+
+    .second-section-paragraph-holder {
+        max-width: 95%;
+        padding-top: 1rem;
+        p {
+            font: {
+                size: 10px;
+                weight: 400;
+            }
+        }
+    }
+
+    .second-section-top-image {
+        width: 43%;
+        height: fit-content;
+        border-radius: 3px;
+        line-height: 0;
+        border: 5px solid $primary-light;
+        box-shadow: 5px 5px 3px rgba($darkest, 16%);
+
+        img {
+            max-width: 100%;
+        }
+    }
+
+    .second-section-bottom-image-wrapper {
+        position: relative;
+        width: 82%;
+        margin: 0 auto;
+        padding: 1rem 0;
+        z-index: 3;
+    }
+
+    .second-section-bottom-image {
+        border-radius: 3px;
+        line-height: 0;
+        border: 5px solid $primary-light;
+        box-shadow: 5px 5px 3px rgba($darkest, 16%);
+
+        img {
+            max-width: 100%;
+        }
+    }
+
+    .second-section-decorative-line {
+        position: absolute;
+        width: 0.313rem;
+        height: 100%;
+        top: 0;
+        right: 0.813rem;
+        z-index: 0;
+        background-color: $fancy;
+        box-shadow: 5px 0px 2px rgb(0 0 0 / 16%);
+    }
+}
+
+@media screen and (min-width: 375px) {
+
+    .second-section-paragraph-holder {
+        p {
+            font: {
+                size: 11px;
+            }
+        }
+    }
+}
+
+@media screen and (min-width: 640px) {
+
+    .article{
+        display: none;
+    }
+}
+
+
+
+</style>
