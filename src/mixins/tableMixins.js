28171f06b1bddcabd8738b906cc7e259dd1e3d4f
matthew.chevy <matej.sevcik11@gmail.com>
2021-03-19 10:58:03 +0100

    Remove old code

--- src/mixins/tableMixins.js
+++ src/mixins/tableMixins.js
@@ -11,14 +11,10 @@ export default {
         this.actualyWidnowSize = window.innerWidth
         window.addEventListener('resize', throttle(this.sizeWindow, 350))
     },
-    
+
     methods: {
         sizeWindow(e) {
             return (this.actualyWidnowSize = e.srcElement.innerWidth)
         },
-
-        // scrollTop() {
-        //     return (this.actualyWidnowSize = e.srcElement.innerWidth)
-        // }
     }
 }
