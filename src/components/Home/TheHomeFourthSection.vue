896792f4a90c31502c98592a51c717f61d173738
matthew.chevy <matej.sevcik11@gmail.com>
2021-03-30 18:25:46 +0200

    redesign homepage, new html

---
+++ src/components/Home/TheHomeFourthSection.vue
@@ -0,0 +1,51 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <div class="why-us">
+                <h2>Prečo si vybrať práve nás ?</h2>
+                <p>
+                    Maľovanie so sebou nesie riziká, niektoré sa ukážu až pri
+                    samotnom maľovaní. Za 2 dekády vykononávania nášho remesla
+                    sme si prešli už naozaj všetkým. Zvolíme vždy ten najlepší
+                    spôsob prípravy podkladu. Pleseň, mastný povrch, praskliny,
+                    zadymené alebo mokré steny pre nás nieje problém.
+                </p>
+            </div>
+            <div class="notes-box">
+                <div class="list-box">
+                    <ul>
+                        <li>Široké možnosti maliarskych prác</li>
+                        <li>Bezplatná obhliadka</li>
+                        <li>Záruka je samozrejmosťou</li>
+                        <li>Upratovanie po prácach</li>
+                        <li>20 rokov skúseností</li>
+                        <li>Pracujeme aj počas víkendu</li>
+                        <li>Individálny prístup</li>
+                        <li>Odborne vykonaná práca</li>
+                    </ul>
+                </div>
+
+                <div class="paragraph-box">
+                    <p>
+                        <strong>Liptov a okolie</strong> - miesto kde pôsobíme.
+                        Treba Vám čokoľvek vymalovať ? Neváhajte nás
+                        kontaktovať, tešíme sa na Vás.
+                    </p>
+                    <div class="contact-button">
+                        <a
+                            class="contact-link"
+                            tabindex="1"
+                            @click="$router.push({ path: '/about' })"
+                        >
+                            Kontaktovať
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
