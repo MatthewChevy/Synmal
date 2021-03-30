059c7902734b458d5adbc5e818ae751997d59ab4
matthew.chevy <matej.sevcik11@gmail.com>
2021-03-30 18:21:53 +0200

    new style 0px+

--- src/components/Home/TheHomeThirdSection.vue
+++ src/components/Home/TheHomeThirdSection.vue
@@ -37,5 +37,74 @@
 </template>
 
 <style lang="scss" scoped>
+@media screen and (min-width: 0px) {
+    .sm-screen {
+        display: block;
+    }
+
+    .lg-screen {
+        display: none;
+    }
+
+    .section {
+        position: relative;
+        background-color: $primary-soft;
+        border: {
+            top: 1px solid $primary;
+            bottom: 1px solid $primary;
+        }
+    }
+
+    .container {
+        width: $container;
+        margin: 0 auto;
+    }
+
+    .decorative-bucket-box {
+        align-items: center;
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    .decorative-bucket {
+        position: relative;
+        width: 70%;
+        height: auto;
+        margin: 3rem auto 3rem;
+
+        img {
+            max-width: 100%;
+        }
+    }
+
+    .decorative-bucket-paragraph {
+        width: 100%;
+
+        p {
+            text-align: center;
+            font: {
+                weight: 300;
+            }
+
+            strong {
+                font: {
+                    weight: 500;
+                }
+            }
+        }
+
+        .p-1 {
+            margin: 3rem 0;
+        }
+        .p-2 {
+            margin-bottom: 3rem;
+        }
+    }
+
+    .decorative-line {
+        display: none;
+    }
+}
+
 
 </style>
