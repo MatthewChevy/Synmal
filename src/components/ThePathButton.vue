f86ee11fcbf0f38c9c7054ab5f8a045fa176c37e
matthew.chevy <matej.sevcik11@gmail.com>
2021-03-31 10:16:54 +0200

    Add props

---
+++ src/components/ThePathButton.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="contact-link-button">
+        <a
+            class="contact-link"
+            tabindex="1"
+            @click="$router.push({ path: `/${path}` })"
+        >
+            {{ textButton }}
+        </a>
+    </div>
+</template>
+
+<script>
+
+export default{
+
+    props: {
+        textButton: {
+            type: String,
+        },
+        path: {
+            type: String,
+        }
+    }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+    .contact-link-button {
+        max-width: 12rem;
+        margin: 0 auto;
+        padding: 3px 30px;
+        border-radius: 3px;
+        color: $darkest;
+        background-color: $fancy;
+        cursor: pointer;
+
+        a{
+            font: {
+                size: 16px;
+                weight: 300
+            }
+        }
+    }
+
+    
+
+    .contact-link-button:hover,
+    :focus {
+        transition: .35s;
+        background-color: darken($fancy, 10%);
+    }
+
+</style>
