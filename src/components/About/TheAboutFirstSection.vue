3fb548136759c9cbe98468c1c3194633db6e38fe
matthew.chevy <matej.sevcik11@gmail.com>
2021-04-02 18:28:30 +0200

    Add new box, responsive 0px - 350px

--- src/components/About/TheAboutFirstSection.vue
+++ src/components/About/TheAboutFirstSection.vue
@@ -3,11 +3,13 @@
 
         <div class="top-box">
             <div class="rectangle">
-                <p>
-                    Za roky našej práce máme mnoho spokojných ľudí, ktorým
-                    sme pomohli zveľadiť ich <strong>bytové</strong> a
-                    <strong>nebytové</strong> priestory
-                </p>
+                <div class="rectangle-align">
+                    <p>
+                        Za roky našej práce máme mnoho spokojných ľudí, ktorým
+                        sme pomohli zveľadiť ich <strong>bytové</strong> a
+                        <strong>nebytové</strong> priestory
+                    </p>
+                </div>
             </div>
         </div>
 
@@ -48,142 +50,38 @@
 
 <style lang="scss" scoped>
 
-//first-section-----------------------------
+@media screen and ( min-width: 0px ) {
 
-.section {
-    position: relative;
-    width: 100%;
-    background: $primary-light;
-    border-radius: 10px 10px 0 0;
-}
-
-.container {
-    width: $sm-width-container;  
-    margin: 0 auto;
-}
-
-.top-box {
-   padding-top: 2rem;
-   width: 100%;
-
-    .rectangle {
+    .section {
         position: relative;
-        display: flex;
-        align-items: center;
         width: 100%;
-        padding: 0.75rem 0;
-        background-color: $primary;
-        border-radius: 5px 0 0 5px;
-
-        p {
-            width: 100%;
-            position: relative;
-            text-align: center;
-            margin: 0 auto;
-            font: {
-                size: 14px;
-                weight: 300;
-            }
-
-            strong {
-                font-weight: 500;
-            }
-        }
-    
+        background: $primary-light;
+        border-radius: 10px 10px 0 0;
     }
-}
 
-.bottom-box {
-
-    .rectangle {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 62.5%;
-        max-height: 4.7rem;
-        margin: 3rem 0;
-        padding: 1rem;
-        z-index: 0;
-        background-color: $fancy;
-        box-shadow: 5px 5px 6px rgba($darkest, 16%);
-
-        p {
-            position: relative;
-            width: 10.563rem;
-            text-align: center;
-
-            font: {
-                size: 14px;
-                weight: 300;
-            }
-        }
-    
-        .decorative-paint-roller {
-            position: absolute;
-            left: 65%;
-            width: 4.9rem;
-            transform: rotate3d(1, 1, 0, 180deg);
-            img {
-                width: 100%;
-            }
-        }
+    .container {
+        width: $sm-width-container;  
+        margin: 0 auto;
     }
-}
 
-.article-box{
-    position: relative;
+    .top-box {
+    padding-top: 3rem;
     width: 100%;
-    padding-top: 0.563rem;
-    padding-bottom: 3rem;
-    text-align: center;
-    box-shadow: 5px 5px 6px rgba($darkest, 16%);
-
-    .article-h2 {
-        h2 {
-            font: {
-                size: 25px;
-                weight: 400;
-            }
-        }
-    }
-
-    .article-paragraph {
-        width: $sm-width-container;
-        margin: 2rem auto 0 auto;
-
-        p {
-            margin: 0 auto;
-            text-align: center;
-            font: {
-                size: 14px;
-                weight: 300;
-            }
-
-            strong {
-                font-weight: 500;
-            }
-        }
-    }
-}
-
-
-@media screen and ( min-width: 375px ) {
-
-    .top-box  {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
 
         .rectangle {
-            width: 85%;
-            padding:{
-                top: 1rem;
-                bottom: 1rem;
-                right: 5%;
-            }
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            padding: 0.75rem 0;
+            background-color: $primary;
+            border-radius: 5px 0 0 5px;
+
             p {
+                width: 100%;
                 position: relative;
-                text-align: right;
+                text-align: center;
                 margin: 0 auto;
                 font: {
                     size: 14px;
@@ -194,6 +92,105 @@
                     font-weight: 500;
                 }
             }
+        
+        }
+    }
+
+    .bottom-box {
+
+        .rectangle {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 62.5%;
+            max-height: 4.7rem;
+            margin: 3rem 0;
+            padding: 1rem;
+            z-index: 0;
+            background-color: $fancy;
+            box-shadow: 5px 5px 6px rgba($darkest, 16%);
+
+            p {
+                position: relative;
+                width: 10.563rem;
+                text-align: center;
+
+                font: {
+                    size: 14px;
+                    weight: 300;
+                }
+            }
+        
+            .decorative-paint-roller {
+                position: absolute;
+                left: 65%;
+                width: 4.9rem;
+                transform: rotate3d(1, 1, 0, 180deg);
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
+
+    .article-box{
+        position: relative;
+        width: 100%;
+        padding-top: 0.563rem;
+        padding-bottom: 3rem;
+        text-align: center;
+        box-shadow: 5px 5px 6px rgba($darkest, 16%);
+
+        .article-h2 {
+            h2 {
+                font: {
+                    size: 25px;
+                    weight: 400;
+                }
+            }
+        }
+
+        .article-paragraph {
+            width: $sm-width-container;
+            margin: 2rem auto 0 auto;
+
+            p {
+                margin: 0 auto;
+                text-align: center;
+                font: {
+                    size: 14px;
+                    weight: 300;
+                }
+
+                strong {
+                    font-weight: 500;
+                }
+            }
+        }
+    }
+}
+
+@media screen and ( min-width: 350px ) {
+
+    .top-box  {
+        .rectangle { 
+            width: 100%;
+            padding: 1rem 0;
+               
+            .rectangle-align{
+                    width: $sm-width-container;
+                    max-width: 25rem;
+                p {
+                    font: {
+                        size: 16px;
+                        weight: 300;
+                    }
+
+                    strong {
+                        font-weight: 500;
+                    }
+                }
+            }
         }
         
     }
@@ -201,28 +198,10 @@
     .bottom-box {
 
     .rectangle {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        width: 63%;
-        margin: 3rem 0;
-        padding:{
-                top: 1rem;
-                bottom: 1rem;
-                left: 5%;
-                right: 0;
-        }
-        z-index: 0;
-        background-color: $fancy;
-        box-shadow: 5px 5px 6px rgba($darkest, 16%);
-
         p {
-            position: relative;
-            width: 100%;
-            text-align: left;
-
+            width: 12rem;
             font: {
-                size: 14px;
+                size: 16px;
                 weight: 300;
             }
         }
@@ -241,5 +220,6 @@
     }
     
 }
+
 
 </style>
