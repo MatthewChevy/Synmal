413b0b321ea45e32b128ec131b6fac8cc5e162dd
matthew.chevy <matej.sevcik11@gmail.com>
2021-04-06 15:15:15 +0200

    update dont use image

--- src/components/About/TheAboutThirdSection.vue
+++ src/components/About/TheAboutThirdSection.vue
@@ -15,51 +15,50 @@
             </div>
         </article>
 
-        <div class="container">
-
-            <article class="article-first-box">
-                <p>
-                    <strong>Pred maľovaním:</strong> Všetko dôkladne zalepíme a pozakrývame fóliou. Tento proces bývá časovo najnáročnejší.
-                    Pri strojovom maľovaní si lepenie vyžaduje ešte väčšiu dôslednosť.
-                </p>
-                <div class="image-before-paint-box">
-                    <div class="image-holder">
-                        <img src="../../assets/img/gallery/123965699_688014581857556_6128033632594933054_n.jpg" alt="before">
+        <div class="flex-box">
+            <article class="article-box">
+                <div class="center">
+                    <p>
+                        <strong>Pred maľovaním:</strong> Všetko dôkladne zalepíme a pozakrývame fóliou. Tento proces bývá časovo najnáročnejší.
+                        Pri strojovom maľovaní si lepenie vyžaduje ešte väčšiu dôslednosť.
+                    </p>
+                    <div class="article-image">
+                        <div class="image-holder">
+                            <img src="../../assets/img/gallery/123965699_688014581857556_6128033632594933054_n.jpg" alt="before">
+                        </div>
                     </div>
                 </div>
             </article>
-        </div>
 
-        <article class="article-second-box">
-            <div class="center">
-                    <div class="second-box-paragraph">
-                        <p>
-                            <strong>Počas maľovania:</strong> V tejto fáze sa ukážu všetky nedostatky ako napríklad praskliny na stenách, strope
-                            alebo okolo okien. Všetko čo treba povyspravujeme a zasádrujeme.
-                        </p>
-                    </div>
-                    <div class="image-painting-box">
+            <article class="article-box">
+                <div class="center">
+                    <p>
+                        <strong>Počas maľovania:</strong> V tejto fáze sa ukážu všetky nedostatky ako napríklad praskliny na stenách, strope
+                        alebo okolo okien. Všetko čo treba povyspravujeme a zasádrujeme.
+                    </p>
+                    <div class="article-image">
                         <div class="image-holder">
                             <img src="../../assets/img/gallery/123965699_688014581857556_6128033632594933054_n.jpg" alt="in progress">
                         </div>
                     </div>
                 </div>
-        </article>
+            </article>
 
-        <article class="article-bottom">
-            <div class="article-bottom-box">
-                <p>
-                   <strong>Po maľovaní:</strong> Ak sme s výsledkom spokojný môžeme začať s upratovacími prácami. 
-                    Všetko dôklade vyzametáme, poumývame a povysávame.
-                </p>
-                <div class="image-after-paint-box">
-                    <div class="image-holder">
-                        <img src="../../assets/img/gallery/IMG_20200824_122141.jpg" alt="after">
+            <article class="article-box last">
+                <div class="center">
+                    <p>
+                    <strong>Po maľovaní:</strong> Ak sme s výsledkom spokojný môžeme začať s upratovacími prácami. Všetko dôklade vyzametáme, poumývame a povysávame. 
+                    Po skončení sa možete tešiť z vymalovných priestorov.
+                    </p>
+                    <div class="article-image">
+                        <div class="image-holder">
+                            <img src="../../assets/img/gallery/123965699_688014581857556_6128033632594933054_n.jpg" alt="after">
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
-
+            </article>
+        </div>
+        
     </section>
     
 </template>
@@ -69,6 +68,11 @@
 
 @media screen and ( min-width: 0px ) {
     
+    .center{
+        width: $sm-width-container;
+        margin: 0 auto;
+    }
+
     .section {
         position: relative;
         width: 100%;
@@ -80,21 +84,18 @@
         background: $primary-light;
         z-index: 0;
         border-bottom: 1px solid $primary;
+        padding-bottom: 2rem;
 
-        .center{
-            width: $sm-width-container; 
-            margin: 0 auto;
-            padding-bottom: 2rem;
+        .article-title {
+            padding: 0;
+            text-align: center;
 
-            .article-title {
-                padding: 0;
-                text-align: center;
-                h2 {
-                    font: {
-                        size: 30px;
-                        weight: 300;
-                    }
+            h2 {
+                font: {
+                    size: 30px;
+                    weight: 300;
                 }
+            }
 
             h2::before,
             h2::after{
@@ -115,34 +116,31 @@
             }
         }
 
-            p {
-                padding: 2rem 0 1rem 0;
-                text-align: justify;
-                font: {
-                    size: 14px;
-                    weight: 300;
-                }
-                strong {
-                    font:{
-                        weight: 500;
-                    }
+        p {
+            padding: 2rem 0 1rem 0;
+            text-align: justify;
+            font: {
+                size: 14px;
+                weight: 300;
+            }
+            strong {
+                font:{
+                    weight: 500;
                 }
             }
         }
     }
 
-    .container {
-        position: relative;
-        width: $sm-width-container;
-        margin: 0 auto;
+    .flex-box{
+        padding-top: 2rem;
     }
 
-    .article-first-box {
+    .article-box {
         position: relative;
         width: 100%;
 
         p {
-            padding-top: 2rem;
+            // padding-top: 2rem;
             text-align: justify;
             font: {
                 size: 14px;
@@ -156,112 +154,15 @@
             }
         }
 
-        .image-before-paint-box {
+        .article-image {
             padding: 2rem 0;
-            
-            .image-holder{
-    
+
                img{
                    width: 100%;
                }
-           }
         }
+        
     }
-
-    .article-second-box {
-
-        position: relative;
-        z-index: 1;
-        background-color: $primary-light;
-        border-radius: 0 0 20px 20px;
-        box-shadow: 8px 8px 5px rgba($darkest, 16%);
-
-        .center{
-            width: $sm-width-container;
-            margin: 0 auto;
-        }
-
-        .second-box-paragraph {
-            margin: 0 auto;
-            padding: 2rem 0;
-            text-align: justify;
-            p {
-                margin: 0 auto;
-                font: {
-                    size: 14px;
-                    weight: 300;
-                }
-
-                strong {
-                    font:{
-                        weight: 500;
-                    }
-                }
-            }
-        }
-
-        .image-painting-box {    
-            padding-bottom: 2rem;
-
-            .image-holder{
-                img{
-                    width: 100%;
-                }
-            }
-        }
-
-        .article-bottom-decoration-line-vr {
-            position: absolute;
-            width: 0.313rem;
-            height: 50%;
-            bottom: 0;
-            left: 4rem;
-            z-index: 2;
-            background-color: $fancy;
-            box-shadow: 5px 0px 2px rgba($darkest, 16%);
-        }
-    }
-
-    .article-bottom {
-        position: relative;
-        width: 100%;
-        top: -1.5rem;
-        padding-top: 1.5rem;
-        padding-bottom: 2rem;
-        background-color: $primary-soft;
-        p {
-            text-align: justify;
-            padding-top: 2rem;
-            margin: 0 auto;
-            font: {
-                size: 14px;
-                weight: 300;
-            }
-            color: $darkest;
-            strong {
-                font:{
-                    weight: 500;
-                }
-            }
-        }
-
-        .article-bottom-box {
-            max-width: $sm-width-container;
-            margin: 0 auto;
-        }
-        .image-after-paint-box{
-            width: 100%;   
-            padding-top: 2rem;
-
-            .image-holder{
-                img{
-                    width: 100%;
-                }
-            }
-        }
-            
-    }
-
 }
 
 @media screen and ( min-width: 350px ) {
@@ -276,9 +177,7 @@
         }
     }
 
-    .article-first-box {
-        position: relative;
-        width: 100%;
+    .article-box {
         p {
             font: {
                 size: 16px;
@@ -287,27 +186,207 @@
         }
     }
 
-    .article-second-box {
-        .second-box-paragraph {
-            p {
+}
+
+@media screen and ( min-width: 768px ) {
+    
+    .center{
+        width: unset;
+        margin: 0 auto;
+    }
+
+    .article-top {
+        width: 100%;
+        background: $primary-light;
+        z-index: 0;
+        border-bottom: 1px solid $primary;
+        padding-bottom: 2rem;
+
+        .article-title {
+            padding: 0;
+            text-align: center;
+
+            h2 {
                 font: {
-                    size: 16px;
+                    size: 30px;
+                    weight: 300;
+                }
+            }
+
+            h2::before,
+            h2::after{
+                content: "";
+                display: inline-block;
+                position: relative;
+                width: 19%;
+                height: 2px;
+                top: -0.4rem;
+                background: $fancy;
+            }
+            
+            h2::before{
+                right: 1rem;
+            }
+            h2:after{
+                left: 1rem;
+            }
+        }
+
+        p {
+            width: 37rem;
+            margin: 0 auto;
+            padding: 1rem 0 1rem 0;
+            text-align: center;
+            font: {
+                size: 14px;
+                weight: 300;
+            }
+            strong {
+                font:{
+                    weight: 500;
                 }
             }
         }
     }
 
-    .article-bottom{
 
-        .article-bottom-box{
-            p{
+    .article-box{
+        width: 37rem;
+        p{
+            text-align: center;
+        }
+    }
+
+    .flex-box{
+        display: flex;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    .last{
+        margin: 0 auto;
+    }
+
+    
+}
+
+@media screen and ( min-width: 1024px ) {
+
+    .center{
+        width: unset;
+        margin: 0 auto;
+    }
+
+    .article-top {
+        width: 100%;
+        background: $primary-light;
+        z-index: 0;
+        border-bottom: 1px solid $primary;
+        padding: 3rem 0 5rem 0;
+
+        .article-title {
+            padding: 0;
+            text-align: center;
+
+            h2 {
+                font: {
+                    size: 30px;
+                    weight: 300;
+                }
+            }
+
+            h2::before,
+            h2::after{
+                content: "";
+                display: inline-block;
+                position: relative;
+                width: 14rem;
+                height: 2px;
+                top: -0.4rem;
+                background: $fancy;
+            }
+            
+            h2::before{
+                right: 1rem;
+            }
+            h2:after{
+                left: 1rem;
+            }
+        }
+
+        p {
+            width: 37rem;
+            margin: 0 auto;
+            padding: 1rem 0 1rem 0;
+            text-align: center;
+            font: {
+                size: 14px;
+                weight: 300;
+            }
+            strong {
                 font:{
-                    size: 16px;
+                    weight: 500;
                 }
             }
         }
+    }
+
+    .article-box{
+        width: 19rem;
+        p{
+            text-align: center;
+            width: 98%;
+            margin: 0 auto;
+        }
+    }
+
+    .flex-box{
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        width: $lg-width-container;
+        margin: 0 auto;
+        padding: 4rem 0;
+    }
+
+    .last{
+        margin: unset
     }
 
 }
+
+@media screen and ( min-width: 1200px ) {
+
+    .article-box{
+        width: 21rem;
+        p{
+            text-align: center;
+            width: 100%;
+            margin: 0 auto;
+        }
+    }
+
+    .flex-box{
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        width: 70rem;
+        margin: 0 auto;
+        padding: 4rem 0;
+    }
+
+    .last{
+        margin: unset
+    }
+
+    
+}
+
+
+
+
+
 
 </style>
