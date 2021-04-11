8438c48413a34bd97d4c18ff92b32727f8f00237
matthew.chevy <matej.sevcik11@gmail.com>
2021-04-11 10:31:31 +0200

    remove pathbutton from ftr was used in 3rd section

--- src/components/About/TheAboutFooter.vue
+++ src/components/About/TheAboutFooter.vue
@@ -1,33 +1,15 @@
 <template>
     <div class="footer-center">
-        <div class="button">
-            <the-path-button
-                :path="'contact'"
-                :textButton="'Kontaktujte nás'"
-            />
-        </div>
     </div>
 </template>
 
-<script>
-import ThePathButton from '../ThePathButton.vue'
-
-export default {
-    components: {
-        ThePathButton
-    }
-}
-</script>
 
 <style lang="scss" scoped>
+
 @media screen and (min-width: 0px) {
+
     .footer-center {
         max-width: 18.75rem;
-        margin: 0 auto;
-    }
-
-    .button {
-        text-align: center;
         margin: 0 auto;
     }
 }
