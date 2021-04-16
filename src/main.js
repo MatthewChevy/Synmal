4e0e57ae32f6f8612b88691d77284a21d2c4b770
matthew.chevy <matej.sevcik11@gmail.com>
2021-04-16 19:50:34 +0200

    install scroll library

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import router from './router'
 import mitt from 'mitt'
 window.eventBus = mitt()
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
 createApp(App)
     .use(router)
     .mount('#app')
