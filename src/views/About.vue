5de074d4fea04d4ce91f66d698843a64dbdffd04
matthew.chevy <matej.sevcik11@gmail.com>
2021-05-03 20:01:58 +0200

    header deleted

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,4 @@
 <template>
-    <header>
-        <the-about-header />
-    </header>
-
     <main>
         <the-about-first-section />
 
@@ -15,7 +11,6 @@
 </template>
 
 <script>
-import TheAboutHeader from '../components/About/TheAboutHeader'
 import TheAboutFirstSection from '../components/About/TheAboutFirstSection'
 import TheAboutSecondSection from '../components/About/TheAboutSecondSection'
 import TheAboutBackgroundPhotoSection from '../components/About/TheAboutBackgroundPhotoSection'
@@ -23,31 +18,10 @@ import TheAboutThirdSection from '../components/About/TheAboutThirdSection'
 
 export default {
     components: {
-        TheAboutHeader,
         TheAboutFirstSection,
         TheAboutSecondSection,
         TheAboutBackgroundPhotoSection,
-        TheAboutThirdSection,
+        TheAboutThirdSection
     }
 }
 </script>
-
-<style lang="scss" scoped>
-// body{
-//     background-color: $primary-semi-light;
-// }
-
-header {
-    padding-top: 3.4rem;
-    background-color: $primary-light;
-    position: relative;
-    z-index: 1;
-}
-
-footer {
-    position: relative;
-    width: 100%;
-    padding: 1rem 0 3rem 0;
-    background-color: $primary-semi-light;
-}
-</style>
